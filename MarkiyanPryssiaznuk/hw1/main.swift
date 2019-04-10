5207b9eb2d35ed6e28642d9ad10f6dc51d8fc799
Markiyan Prysiazhniuk <pryssiaznuk12@gmail.com>
2019-04-10 21:05:52 +0300

    Repair my hw

--- MarkiyanPryssiaznuk/hw1/main.swift
+++ MarkiyanPryssiaznuk/hw1/main.swift
@@ -9,11 +9,15 @@
 import Foundation
 
 enum BookType: String {
-    case jornal
-    case book
-    case fiction
-    case dictionary
-    case logbooks
+    case jornal = "Journal"
+    case book = "Book"
+    case fiction = "Fiction"
+    case dictionary = "Dictionary"
+    case logbooks = "Logbooks"
+}
+
+enum BookState {
+    case some(status: String)
 }
 
 enum filterType {
@@ -28,8 +32,8 @@ enum booksError: Error {
 
 struct Order: Hashable {
     var book: Book
-    var human: Human
-    var date: Date
+    var human: Human?
+    var date: Date?
     
     static func == (lhs: Order, rhs: Order) -> Bool {
         return lhs.book == rhs.book && lhs.date == rhs.date && lhs.human == rhs.human
@@ -47,13 +51,13 @@ struct Book: Hashable, Comparable {
     let author: String
     let type: BookType
     let name: String
-    var status: filterType
+    var status: BookState
     
     init(author: String, type: BookType, name: String) {
         self.name = name
         self.author = author
         self.type = type
-        self.status = .available
+        self.status = .some(status: "available")
     }
     
     func hash(into hasher: inout Hasher) {
@@ -61,7 +65,6 @@ struct Book: Hashable, Comparable {
         hasher.combine(author)
         hasher.combine(name)
         hasher.combine(type)
-        hasher.combine(status)
     }
     
     static func == (lhs: Book, rhs: Book) -> Bool {
@@ -104,19 +107,20 @@ class Library {
         case byDate
         case byHuman
     }
+
+    private var takenBooks = Set<Order>()
+    private var balanceOfBooks = Set<Order>()
     
-    var takenBooks = Set<Order>()
-    var balanceOfBooks: [Book] = []
-    var recivedBooks = Set<Order>()
-    
-    var historyOfTaken: [String : Order] = [:]
-    var historyOfRecieved: [String : Order] = [:]
+    private var historyOfTaken: [String : Order] = [:]
+    private var historyOfRecieved: [String : Order] = [:]
     
 }
+
 extension Library {
     final func addNewBook(book: Book) {
-        balanceOfBooks.append(book)
-        print("our new book \(book.author) \(book.name) \(book.type)")
+        let newOrder = Order(book: book, human: nil, date: nil)
+        balanceOfBooks.insert(newOrder)
+        print("our new book \(newOrder.book.author) \(newOrder.book.name) \(newOrder.book.type)")
     }
     
     final func takeBook(book: Book, human: Human) throws {
@@ -125,25 +129,32 @@ extension Library {
             print("Book balance is empty")
             throw booksError.someError(error: "Balance of books aren't good")
         } else {
-            balanceOfBooks.removeAll { $0.uuid == book.uuid }
+            for item in balanceOfBooks {
+                if newOrder.book.uuid.hashValue == item.book.uuid.hashValue {
+                    balanceOfBooks.remove(item)
+                }
+            }
         }
+        newOrder.book.status = .some(status: "taken")
         takenBooks.insert(newOrder)
-        newOrder.book.status = .taken
         historyOfTaken[book.uuid] = newOrder
     }
     
     final func recieveBook(book: Book? , human: Human) throws {
         guard let newBook = book else { throw booksError.someError(error: "Book is nil") }
         var newOrder = Order(book: newBook, human: human, date: Date())
-        recivedBooks.insert(newOrder)
-        balanceOfBooks.append(newBook)
-        newOrder.book.status = .available
+        for item in takenBooks {
+            if newOrder.book.uuid.hashValue == item.book.uuid.hashValue {
+                takenBooks.remove(item)
+            }
+        }
+        balanceOfBooks.insert(newOrder)
+        newOrder.book.status = .some(status: "available")
         historyOfRecieved[book!.uuid] = newOrder
     }
 }
 
 extension Library {
-    
     func filer(filet: filterType) {
         switch filet {
         case .available:
@@ -159,7 +170,11 @@ extension Library {
 extension Library {
     func printTakenBooks() {
         for item in takenBooks {
-                print("book with id: \(item.book.uuid) was taken his author: \(item.book.author) | \n name: \(item.book.name) | \n status: \(item.book.status) | \n type: \(item.book.type) | \n it was \(item.book.status) at: \(item.date) | \n by: \(item.human.name) | \n passport: \(item.human.passport)")
+            let book = item.book
+            guard let human = item.human else { return }
+            guard let date = item.date else { return }
+
+                print("book with id: \(book.uuid) was taken his author: \(book.author) | \n name: \(book.name) | \n status: \(book.status) | \n type: \(book.type) | \n it was \(book.status) at: \(date) | \n by: \(human.name) | \n passport: \(human.passport)")
             }
     }
     
@@ -167,26 +182,32 @@ extension Library {
         let calendar = Calendar.current
         
         for (key, value) in historyOfTaken {
+            let dictBook = value.book
+            guard let human = value.human else { return }
+            guard let date = value.date else { return }
             if value.book.uuid == book.uuid {
-                 let components = calendar.dateComponents([.day], from: value.date, to: Date())
-                  print("book with id: \(key) has history  author: \(value.book.author) | \n name: \(value.book.name) | \n status: \(value.book.status) | \n type: \(value.book.type) | \n it was \(value.book.status) at: \(value.date) to \(components) | \n by: \(value.human.name)  | \n passport: \(value.human.passport)")
+                guard let dateFrom = value.date else { return }
+                 let components = calendar.dateComponents([.day], from: dateFrom, to: Date())
+                  print("book with id: \(key) has history  author: \(dictBook.author) | \n name: \(dictBook.name) | \n status: \(dictBook.status) | \n type: \(dictBook.type) | \n it was \(dictBook.status) at: \(date) to \(components) | \n by: \(human.name)  | \n passport: \(human.passport)")
             }
         }
         
         for (key, value) in historyOfRecieved {
+            let dictBook = value.book
+            guard let human = value.human else { return }
+            guard let date = value.date else { return }
             if value.book.uuid == book.uuid {
-                let components = calendar.dateComponents([.day], from: value.date, to: Date())
-                print("book with id: \(key) has history  author: \(value.book.author) | \n name: \(value.book.name) | \n status: \(value.book.status) | \n type: \(value.book.type) | \n it was \(value.book.status) at: \(value.date) to \(components) | \n by: \(value.human.name)  | \n passport: \(value.human.passport)")
+                guard let dateFrom = value.date else { return }
+                let components = calendar.dateComponents([.day], from: dateFrom, to: Date())
+                print("book with id: \(key) has history  author: \(dictBook.author) | \n name: \(dictBook.name) | \n status: \(dictBook.status) | \n type: \(dictBook.type) | \n it was \(dictBook.status) at: \(date) to \(components) | \n by: \(human.name)  | \n passport: \(human.passport)")
             }
         }
-        
-        
     }
     
     func printBookBalance() {
         if balanceOfBooks.isEmpty != true {
-            for book in balanceOfBooks {
-                print("now available: \(book.author) \(book.name) \(book.type)")
+            for item in balanceOfBooks {
+                print("now available: \(item.book.author) \(item.book.name) \(item.book.type)")
             }
         } else {
             print("Library doesn't have any available books")
@@ -195,10 +216,19 @@ extension Library {
 }
 
 extension Library {
-    func sort(sort: sortType) -> [Order] {
-        let array: [Order] = Array(recivedBooks)
-        
+    func sort(sort: sortType, filter: filterType) -> [Order] {
+        var array: [Order] = []
         var sorted: [Order] = []
+        
+        switch filter {
+        case .all:
+            array = Array(balanceOfBooks.symmetricDifference(takenBooks))
+        case .available:
+            array = Array(balanceOfBooks)
+        case .taken:
+            array = Array(takenBooks)
+        }
+        
         switch sort {
         case .byAuthor:
             sorted = array.sorted{ $0.book.author < $1.book.author }
@@ -207,9 +237,9 @@ extension Library {
         case .byType:
             sorted = array.sorted{ $0.book.type.rawValue < $1.book.type.rawValue }
         case .byDate:
-            sorted = array.sorted{ $0.date < $1.date }
+            sorted = array.sorted{ $0.date! < $1.date! }
         case .byHuman:
-            sorted = array.sorted{ $0.human.passport < $1.human.passport }
+            sorted = array.sorted{ $0.human!.passport < $1.human!.passport }
         }
         return sorted
     }
@@ -218,17 +248,23 @@ extension Library {
 let book1 = Book(author: "King", type: .book, name: "Cool name")
 let book2 = Book(author: "Marshmelow", type: .fiction, name: "Awesome name")
 let book3 = Book(author: "Abs", type: .jornal, name: "Unbelivable name")
-let book4 = Book(author: "Lolka", type: .fiction, name: "Gorgeous name")
+let book4 = Book(author: "Lolka", type: .dictionary, name: "Gorgeous name")
+
 
 let human1 = Human(name: "Markiayn", surname: "Pryssiaznuk")
 let human2 = Human(name: "Ivan", surname: "Pryssiaznuk")
 
 let library = Library()
 
+print("-------------------------------------- Created books --------------------------------------")
+print()
+
 library.addNewBook(book: book1)
 library.addNewBook(book: book2)
 library.addNewBook(book: book3)
 library.addNewBook(book: book4)
+
+
 
 print()
 print("-------------------------------------- Added books --------------------------------------")
@@ -249,7 +285,7 @@ library.printTakenBooks()
 
 
 print()
-print("-------------------------------------- new available: --------------------------------------")
+print("-------------------------------------- now available: --------------------------------------")
 library.printBookBalance()
 print()
 
@@ -274,6 +310,9 @@ library.printHistory(book: book1)
 
 print("-------------------------------------- Sorted by type: --------------------------------------")
 print()
-for item in library.sort(sort: .byType) {
-    print("book with id: was taken his author: \(item.book.author) | \n name: \(item.book.name) | \n status: \(item.book.status) | \n type: \(item.book.type) | \n it was \(item.book.status) at: \(item.date) | \n by: \(item.human.name) | \n passport: \(item.human.passport)")
+for item in library.sort(sort: .byType, filter: .available) {
+    let book = item.book
+    let human = item.human ?? nil
+    let date = item.date ?? nil
+    print("book with id: was taken his author: \(book.author) | \n name: \(book.name) | \n status: \(book.status) | \n type: \(book.type) | \n it was \(book.status) at: \(String(describing: date)) | \n by: \(String(describing: human?.name)) | \n passport: \(String(describing: human?.passport))")
 }
